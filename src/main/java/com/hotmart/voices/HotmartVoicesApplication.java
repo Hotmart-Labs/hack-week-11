89f683609a0c2c8b31ef01c3eaed887224aaa328
Matheus Venâncio <matheus.silva@hotmart.com>
2023-12-05 17:01:03 -0300

    Add initial structure

---
+++ src/main/java/com/hotmart/voices/HotmartVoicesApplication.java
@@ -0,0 +1,13 @@
+package com.hotmart.voices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotmartVoicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotmartVoicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotmart/voices/HotmartVoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotmart.voices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotmartVoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
