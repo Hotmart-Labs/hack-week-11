98fca19fcbacb3aff8b9d34a82a2e6bc08ee36f8
Michel Medeiros <michel.medeiros@hotmart.com>
2023-12-07 16:55:19 -0300

    Upload file based in stream

--- src/main/java/com/hotmart/voices/domain/gateway/ApiElevenGateway.java
+++ src/main/java/com/hotmart/voices/domain/gateway/ApiElevenGateway.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(value = "api-eleven", url = "${feign.client.api-eleven}")
 public interface ApiElevenGateway {
 
-  @PostMapping(path = "/v1/text-to-speech/{voiceId}")
+  @PostMapping(path = "/v1/text-to-speech/{voiceId}/stream")
   byte[] createAudio(@RequestHeader(name = "xi-api-key") String apiToken,
                      @PathVariable(name = "voiceId") String voiceId,
                      @RequestBody ElevenRequestDTO elevenRequestDTO);
